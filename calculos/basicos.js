cc34a09c9794b29c4edf764603cc37060897f255
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2024-08-20 20:44:18 -0300

    adicionadas funções básicas de soma e subtração

--- calculos/basicos.js
+++
@@ -1,8 +0,0 @@
-/**
- * Soma valores numéricos
- * @param  {...number} numeros Valores a serem somados
- * @returns Total da soma
- */
-export function somarNumeros(...numeros) {
-  return numeros.reduce((total, proximo) => total + proximo, 0);
-}
